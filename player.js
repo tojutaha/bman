4b562dde711a967ed5136f27f166068166be9288
Tomi Halko <tomi.halko@gmail.com>
2023-09-11 14:42:22 +0300

    grid movement systeemi

--- player.js
+++ player.js
@@ -1,5 +1,11 @@
 import { canvas, ctx, level, levelHeight, levelWidth, playerSize, tileSize } from "./main.js";
 
+// Tämän kun heittää falseksi, saa sen ensimmäisen version
+// liikkumisen takaisin.
+const useGridMovement = true;
+const gridMovementSpeed = 125; // interval / ms
+const smoothMovementSpeed = 2; // pixels/s
+
 const player = {
     x: 32, // start from top left corner
     y: 32,
@@ -7,10 +13,84 @@ const player = {
     h: 32,
     dx: 0,
     dy: 0,
-    movementSpeed: 2.
 };
 
-export function renderPlayer()
+const Direction = {
+    UP: "Up",
+    DOWN: "Down",
+    LEFT: "Left",
+    RIGHT: "Right",
+}
+
+////////////////////
+// Render/Update
+let movementTimer = null;
+
+function startMovement(direction)
+{
+    if (movementTimer === null) {
+        movementTimer = setInterval(() => {
+            movePlayer(direction);
+        }, gridMovementSpeed);
+    }
+}
+
+function stopMovement()
+{
+    clearInterval(movementTimer);
+    movementTimer = null;
+}
+
+function movePlayer(direction)
+{
+    switch(direction) {
+        case Direction.UP:
+            player.dy = -1;
+            player.dx = 0;
+            break;
+        case Direction.DOWN:
+            player.dy = 1;
+            player.dx = 0;
+            break;
+        case Direction.LEFT:
+            player.dx = -1;
+            player.dy = 0;
+            break;
+        case Direction.RIGHT:
+            player.dx = 1;
+            player.dy = 0;
+            break;
+    }
+
+}
+
+function isWalkable(x, y)
+{
+    if (x < 0 || x >= levelWidth ||
+        y < 0 || y >= levelHeight) {
+        return false;
+    }
+
+    return level[x][y].isWalkable;
+}
+
+function gridMovementUpdatePlayer()
+{
+    let nextX = Math.floor((player.x + player.w / 2) / tileSize);
+    let nextY = Math.floor((player.y + player.h / 2) / tileSize);
+
+    nextX += player.dx;
+    nextY += player.dy;
+
+    if (isWalkable(nextX, nextY)) {
+        player.x = nextX * tileSize;
+        player.y = nextY * tileSize;
+        player.dx = 0;
+        player.dy = 0;
+    }
+}
+
+function smoothMovementUpdatePlayer()
 {
     // Left
     if (player.x <= tileSize) {
@@ -32,8 +112,16 @@ export function renderPlayer()
         player.y = (levelHeight - 2) *tileSize;
     }
 
+    // TODO: loput collision testit...
+
     player.x += player.dx;
     player.y += player.dy;
+}
+
+const updatePlayer = useGridMovement ? gridMovementUpdatePlayer : smoothMovementUpdatePlayer;
+export function renderPlayer()
+{
+    updatePlayer();
 
     ctx.fillStyle = "#ff0000";
     ctx.fillRect(player.x, player.y, playerSize, playerSize);
@@ -41,30 +129,75 @@ export function renderPlayer()
 
 ////////////////////
 // Inputs
-function handleKeyDown(event)
+function gridMovementHandleKeyDown(event)
 {
     event.preventDefault();
 
     switch(event.key) {
         case "w":
-            player.dy = -player.movementSpeed;
+            startMovement(Direction.UP);
             break;
 
         case "a":
-            player.dx = -player.movementSpeed;
+            startMovement(Direction.LEFT);
             break;
 
         case "s":
-            player.dy = player.movementSpeed;
+            startMovement(Direction.DOWN);
             break;
 
         case "d":
-            player.dx = player.movementSpeed;
+            startMovement(Direction.RIGHT);
+            break;
+
+        case " ":
+            console.log("laitappa pommi");
             break;
     }
 }
 
-function handleKeyUp(event)
+function smoothMovementHandleKeyDown(event)
+{
+    event.preventDefault();
+
+    switch(event.key) {
+        case "w":
+            player.dy = -smoothMovementSpeed;
+            break;
+
+        case "a":
+            player.dx = -smoothMovementSpeed;
+            break;
+
+        case "s":
+            player.dy = smoothMovementSpeed;
+            break;
+
+        case "d":
+            player.dx = smoothMovementSpeed;
+            break;
+
+        case " ":
+            console.log("laitappa pommi");
+            break;
+    }
+}
+
+function gridMovementHandleKeyUp(event)
+{
+    event.preventDefault();
+
+    switch(event.key) {
+        case "w":
+        case "s":
+        case "a":
+        case "d":
+            stopMovement();
+            break;
+    }
+}
+
+function smoothMovementHandleKeyUp(event)
 {
     event.preventDefault();
 
@@ -83,6 +216,8 @@ function handleKeyUp(event)
 
 ////////////////////
 // DOM
+const handleKeyDown = useGridMovement ? gridMovementHandleKeyDown : smoothMovementHandleKeyDown;
+const handleKeyUp = useGridMovement ? gridMovementHandleKeyUp : smoothMovementHandleKeyUp;
 document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
 
