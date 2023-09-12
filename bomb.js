50d37602790fda8c829feddfb38ac3724544ca16
MrRodent <finaali@gmail.com>
2023-09-12 17:21:43 +0300

    Tikittävät pommit

--- bomb.js
+++ bomb.js
@@ -1,19 +1,49 @@
 import { ctx, level, tileSize, spriteSheet } from "./main.js";
 import { player } from "./player.js";
  
+function Bomb(x, y, hasExploded, bombTicks) {
+    this.x = x || 0;
+    this.y = y || 0,
+    this.hasExploded = this.hasExploded || false;
+    this.ticks = bombTicks || 4;
+
+    let ticking = setInterval(() => {
+        this.ticks--;
+        if (this.ticks === 0) {
+            clearInterval(ticking);
+            hasExploded = true;
+        }
+    }, 1000);
+}
+
+let bombs = [];
+
 export function dropBomb() {
     let bombX = Math.round(player.x / 32) * 32;
     let bombY = Math.round(player.y / 32) * 32;
-    console.log(bombX, bombY, "bomb");
-    console.log(player.x, player.y, "player");
-
-    return bombX, bombY;
+    bombs.push(new Bomb(bombX, bombY));
 }
 
 ////////////////////
 // Render
 export function renderBombs() {
-    let bX = 32*2;
-    let bY = 32*1;
-    ctx.drawImage(spriteSheet, 0, 32, 32, 32, bX, bY, tileSize, tileSize);
+    if (bombs.length > 0) {
+        bombs.forEach(bomb => {
+            if (bomb.ticks === 4) {
+                ctx.drawImage(spriteSheet, 0, 32, 32, 32, bomb.x, bomb.y, tileSize, tileSize);
+            }
+            else if (bomb.ticks === 3) {
+                ctx.drawImage(spriteSheet, 32, 32, 32, 32, bomb.x, bomb.y, tileSize, tileSize);
+            }
+            else if (bomb.ticks === 2) {
+                ctx.drawImage(spriteSheet, 64, 32, 32, 32, bomb.x, bomb.y, tileSize, tileSize);
+            }
+            else if (bomb.ticks === 1) {
+                ctx.drawImage(spriteSheet, 96, 32, 32, 32, bomb.x, bomb.y, tileSize, tileSize);
+            }
+            else {
+                ctx.drawImage(spriteSheet, 128, 32, 32, 32, bomb.x, bomb.y, tileSize, tileSize);
+            }
+        });
+    }
 }
