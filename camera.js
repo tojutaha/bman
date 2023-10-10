e8dd47e1974124580d2b5f3bfe2315d873c15852
Tomi Halko <tomi.halko@gmail.com>
2023-10-10 11:13:32 +0300

    camera: smoothimpi siirtymä

--- camera.js
+++ camera.js
@@ -1,22 +1,59 @@
-import { ctx, tileSize, scale, levelWidth, canvas } from "./main.js";
+import { ctx, tileSize, scale, levelWidth, canvas, deltaTime } from "./main.js";
 import { players } from "./player.js";
 import { getTileFromWorldLocation, lerp } from "./utils.js";
 
 let cameraX = 0;
+let cameraLt = 0;
+let cameraRt = 0;
+let cameraPt = 0;
+const followCameraSpeed = 0.05;
+const edgeCameraSpeed = 0.25; // TODO: Parempi vaihtoehto tälle
 export function updateCamera()
 {
+
+    if (isNaN(deltaTime))
+        return;
+
     const playerTile = getTileFromWorldLocation(players[0]);
     const playerX = playerTile.x / tileSize;
 
-
-    // TODO: Smooth transition?!
+    // Left edge
     if (playerX <= 6) {
-        ctx.setTransform(scale, 0, 0, scale, 0, 0);
+        cameraPt = 0;
+
+        cameraLt += deltaTime * edgeCameraSpeed;
+        cameraLt = Math.min(cameraLt, 1);
+        const targetX = 0;
+        cameraX = lerp(cameraX, targetX, cameraLt);
+
+        ctx.setTransform(scale, 0, 0, scale, cameraX, 0);
+
+    // Right edge
     } else if (playerX >= levelWidth - 6) {
-        ctx.setTransform(scale, 0, 0, scale, canvas.width / 2 - scale * (levelWidth - 6) * tileSize, 0);
+        cameraPt = 0;
+
+        cameraRt += deltaTime * edgeCameraSpeed;
+        cameraRt = Math.min(cameraRt, 1);
+
+        const targetX = canvas.width / 2 - scale * (levelWidth - 6) * tileSize;
+        cameraX = lerp(cameraX, targetX, cameraRt);
+
+        ctx.setTransform(scale, 0, 0, scale, cameraX, 0);
+
+    // Follow camera
     } else {
-        ctx.setTransform(scale, 0, 0, scale, canvas.width / 2 - scale * players[0].x, 0);
+        cameraLt = 0;
+        cameraRt = 0;
+
+        cameraPt += deltaTime * followCameraSpeed;
+        cameraPt = Math.min(cameraPt, 1);
+
+        const targetX = canvas.width / 2 - scale * players[0].x;
+        cameraX = lerp(cameraX, targetX, cameraPt);
+
+        ctx.setTransform(scale, 0, 0, scale, cameraX, 0);
     }
 
     //ctx.setTransform(scale, 0, 0, scale, canvas.width/2 - scale * players[0].x, canvas.height/2 - scale * players[0].y);
 }
+
