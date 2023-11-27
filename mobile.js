e5fbafc57eaf9446780f3392f3cf057506463972
MrRodent <finaali@gmail.com>
2023-11-27 18:11:24 +0200

    Mobile canvas resize

--- mobile.js
+++ mobile.js
@@ -1,15 +1,35 @@
 import { globalPause, setGlobalPause } from "./main.js";
 
-window.addEventListener('resize', responsivityCheck);
-
 export function responsivityCheck() {
     const isPortrait = window.matchMedia('(orientation: portrait)').matches;
-
+    const isLandscape = window.matchMedia('(orientation: landscape)').matches;
+    
     if (isPortrait) {
-        console.log("Portrait");
+        // console.log("Portrait");
         // setGlobalPause(!globalPause);    // TODO: ei toimi debuglatauksessa
     } else {
-        console.log("Not portrait");
-    }    
+        // console.log("Not portrait");
+        resizeCanvas();
+    }
+
+    if (isLandscape) {
+        // console.log("landscape");
+    }
 }
 
+/////////////////
+// Resize canvas
+let canvas = document.getElementById('canvas');
+let floor = document.querySelector('.floor');
+
+function resizeCanvas() {
+    const windowHeight = window.innerHeight;
+
+    canvas.width = windowHeight;
+    canvas.height = windowHeight;
+
+    floor.style.width = `${windowHeight}px`
+    floor.style.height = `${windowHeight}px`
+}
+
+window.addEventListener('resize', responsivityCheck);
