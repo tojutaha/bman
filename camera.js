da1bdaa648117d25b311326fd01c43abe19ab732
MrRodent <finaali@gmail.com>
2023-12-01 14:17:28 +0200

    getTargetX

--- camera.js
+++ camera.js
@@ -15,6 +15,20 @@ export function setCameraY(value) {
     cameraY = value;
 }
 
+let targetOffset;
+let edgeOffset;
+export function setCameraOffsets() {
+    if (isMobile) {
+        console.info("camera: mobile offsets");
+        targetOffset = 4;
+        edgeOffset = 4;
+    } else {
+        console.info("camera: normal offsets");
+        targetOffset = 6.5;
+        edgeOffset = 6;
+    }
+}
+
 let cameraLt = 0;
 let cameraRt = 0;
 let cameraPt = 0;
@@ -24,70 +38,51 @@ let cameraT = 0;
 const followCameraSpeed = 0.5;
 const edgeCameraSpeed = 0.25;
 
-let targetOffset;
-let edgeOffset;
-
 export function updateCamera() {
     const playerTile = getTileFromWorldLocation(players[0]);
     const playerX = playerTile.x / tileSize;
     const playerY = playerTile.y / tileSize;
-    let targetX;
-    let targetY;
 
     // Left edge
     if (playerX <= edgeOffset) {
-        // console.log('left');
         resetOtherTs('Lt');
         cameraLt = getEdgeCameraT(cameraLt);
         cameraT = cameraLt;
-
-        targetX = 0;
-        targetY = getTargetY(playerY);
+        // console.log('left');
 
     // Right edge
     } else if (playerX >= levelWidth - edgeOffset) {
-        // console.log('right');
         resetOtherTs('Rt');
         cameraRt = getEdgeCameraT(cameraRt);
         cameraT = cameraRt;
-
-        targetX = canvas.width / 2 - scale * (levelWidth - targetOffset) * tileSize;
-        targetY = getTargetY(playerY);
+        // console.log('right');
 
     // Top edge
     } else if (playerY <= edgeOffset) {
-        // console.log('top');
         resetOtherTs('Tt');
         cameraTt = getEdgeCameraT(cameraTt);
         cameraT = cameraTt;
-
-        targetX = canvas.width / 2 - scale * players[0].x;
-        targetY = 0;
+        // console.log('top');
 
     // Bottom edge
     } else if (playerY >= levelHeight - edgeOffset) {
-        // console.log('bot');
         resetOtherTs('Bt');
         cameraBt = getEdgeCameraT(cameraBt);
         cameraT = cameraBt;
+        // console.log('bot');
 
-        targetX = canvas.width / 2 - scale * players[0].x;
-        targetY = getTargetY(playerY);
-    
     // Follow camera
     } else {
-        // console.log('follow');
         resetOtherTs('Pt');
-
         cameraPt += deltaTime * followCameraSpeed;
         cameraPt = Math.min(cameraPt, 1);
         cameraT = cameraPt;
-
-        targetX = canvas.width / 2 - scale * players[0].x;
-        targetY = getTargetY(playerY);
+        // console.log('follow');
     }
 
     // Lerp and transform
+    const targetX = getTargetX(playerX);
+    const targetY = getTargetY(playerY);
     cameraX = lerp(cameraX, targetX, cameraT);
     cameraY = lerp(cameraY, targetY, cameraT);
     ctx.setTransform(scale, 0, 0, scale, cameraX, cameraY);
@@ -107,6 +102,21 @@ function getEdgeCameraT(t) {
     return Math.min(t, 1);
 }
 
+function getTargetX(playerX) {
+    let targetX;
+    // Right edge
+    if (playerX <= edgeOffset) {
+        targetX = 0;
+    }
+    // Left edge
+    else if (playerX >= levelWidth - edgeOffset) {
+        targetX = canvas.width / 2 - scale * (levelWidth - targetOffset) * tileSize;
+    } else {
+        targetX = canvas.width / 2 - scale * players[0].x;
+    }
+    return targetX;
+}
+
 // TODO: enää napsahtaa sivuedge -> Y suunnassa
 function getTargetY(playerY) {
     let targetY;
@@ -116,21 +126,9 @@ function getTargetY(playerY) {
     } 
     // Bottom edge
     else if (playerY >= levelHeight - edgeOffset) { 
-        targetY = canvas.width / 2 - scale * (levelHeight - targetOffset) * tileSize;
+        targetY = canvas.height / 2 - scale * (levelHeight - targetOffset) * tileSize;
     } else {
         targetY = canvas.height / 2 - scale * players[0].y;
     }
     return targetY;
-}
-
-export function setCameraOffsets() {
-    if (isMobile) {
-        console.info("camera: mobile offsets");
-        targetOffset = 4;
-        edgeOffset = 4;
-    } else {
-        console.info("camera: normal offsets");
-        targetOffset = 6.5;
-        edgeOffset = 6;
-    }
 }
